7f7ccb6d77dccc76467851d0959c9795f9ba0dbf
Vincent <vincentmattes@gmail.com>
2022-06-02 17:40:18 +0200

    versuch das create fenster zu bauen

--- app/src/main/java/com/tutorialapp/feature/create/Create.kt
+++ app/src/main/java/com/tutorialapp/feature/create/Create.kt
@@ -1,15 +1,47 @@
 package com.tutorialapp.feature.create
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun Create(){
-    var value = "Tutorial Name"
-    TextField(
+
+    Column {
+        tutorialNameTextField()
+        //Divider(color = Color.Blue, thickness = 1.dp)
+        Column{
+            addStep()
+        }
+    }
+
+
+
+
+}
+
+@Composable
+fun tutorialNameTextField(){
+    var value by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
         value = value,
-        onValueChange = {value = it},
-        label = { Text("Enter the name of the tutorial")}
+        onValueChange = { newText ->
+            value = newText
+        },
+        label = { Text(text = "Name of the tutorial")},
+        maxLines = 1,
+    )
+}
+@Composable
+fun addStep(){
+    var content by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = content,
+        onValueChange = {content = it},
+        label = { Text("Describe this step")},
+
     )
 }
