c04c15adbf767aee063c8599b38938ff890b7318
Vincent <vincentmattes@gmail.com>
2022-06-24 22:31:45 +0200

    the database now gets initialized, i had to add the class to the manifest.xml

--- app/src/main/java/com/tutorialapp/App.kt
+++ app/src/main/java/com/tutorialapp/App.kt
@@ -3,18 +3,31 @@ package com.tutorialapp
 import android.app.Application
 import androidx.room.Room
 import com.tutorialapp.data.database.AppDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
 
 class App : Application() {
+
+    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main)
 
     override fun onCreate(){
         super.onCreate()
         database = Room
             .databaseBuilder(this, AppDatabase::class.java, "app")
-            //.apply {
-            //    if (BuildConfig.DEBUG) fallbackToDestructiveMigration()
-            //}
+            .apply {
+                if (BuildConfig.DEBUG) fallbackToDestructiveMigration()
+            }
             .build()
+
+
+        scope.launch {
+
+        }
+
     }
+
 
     companion object{
         lateinit var database: AppDatabase
--- app/src/main/java/com/tutorialapp/feature/open/OpenScreen.kt
+++ app/src/main/java/com/tutorialapp/feature/open/OpenScreen.kt
@@ -1,18 +1,31 @@
 package com.tutorialapp.feature.open
 
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.tutorialapp.domain.Tutorial
-import com.tutorialapp.domain.allTutorials
+import com.tutorialapp.data.database.Tutorial
 
 @Composable
-fun OpenScreen(){
+fun OpenScreen(viewModel: OpenScreenViewModel = androidx.lifecycle.viewmodel.compose.viewModel()){
+    val tutorials by viewModel.bindUi(LocalContext.current).observeAsState(emptyList())
+    OpenScreenUI(tutorials)
     Open()
 }
-
+@Composable
+private fun OpenScreenUI(tutorials: List<Tutorial>){
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState) {
+        itemsIndexed(tutorials){ _, item ->
+            Text(item.title)
+        }
+    }
+}
 @Preview
 @Composable
 fun OpenScreen_Preview() {
