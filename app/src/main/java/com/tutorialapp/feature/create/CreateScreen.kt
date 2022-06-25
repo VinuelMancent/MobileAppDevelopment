e85fc48bb9e4a84735ca52d1a90666fc8ec97ce0
Vincent <vincentmattes@gmail.com>
2022-06-25 11:39:08 +0200

    title now gets properly read as well, deleted commented code in createScreenVewModel as well as looked for coding conventions

--- app/src/main/java/com/tutorialapp/feature/create/CreateScreen.kt
+++ app/src/main/java/com/tutorialapp/feature/create/CreateScreen.kt
@@ -4,5 +4,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun CreateScreen(viewModel: CreateScreenVewModel = androidx.lifecycle.viewmodel.compose.viewModel()){
-    Create(viewModel::SaveTutorialInLocalDB)
+    Create(viewModel::saveTutorialInLocalDB)
 }
