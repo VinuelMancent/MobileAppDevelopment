e8765abdee445ca0a722478b41d2bb5fcaed5817
s-wdh <56883358+s-wdh@users.noreply.github.com>
2022-06-27 11:05:04 +0200

    Design of Start

--- app/src/main/java/com/tutorialapp/feature/start/Start.kt
+++ app/src/main/java/com/tutorialapp/feature/start/Start.kt
@@ -1,16 +1,26 @@
 package com.tutorialapp.feature.start
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.unit.dp
+import com.tutorialapp.R
 import com.tutorialapp.domain.Tutorial
+import com.tutorialapp.feature.open.uploadButtonToDatabase
 import kotlin.reflect.KFunction2
 
 @Composable
@@ -29,24 +39,50 @@ fun Start(onDownloadButtonClicked: KFunction2<Int, Tutorial, Unit>, onLoadButton
                 if(downloaded){
                     buttonText.value = "downloaded"
                 }
+                /*
                 Row{
                     Text(item.title)
                     System.out.println(item.title)
                     Button(
+                        onClick = { onDownloadButtonClicked(item.id, item) },
+                    ){
+                        Icon(contentDescription = "", painter = painterResource(R.drawable.ic_baseline_file_download_24))
+                    }
+                }
+                 */
+                Box(modifier = Modifier
+                    .padding(5.dp)
+                    .width(380.dp)
+                    .border(width = 2.dp, color = Color.LightGray, shape = RoundedCornerShape(8.dp))
+                    .wrapContentHeight(align = Alignment.CenterVertically))
+                {
+                    Text(item.title,
+                        Modifier
+                            .padding(vertical = 3.dp)
+                            .padding(horizontal = 5.dp),
+                            textDecoration = TextDecoration.Underline)
+                    Text("steps: " + (item.steps.lastIndex + 1).toString(),
+                        Modifier
+                            .padding(top = 25.dp)
+                            .padding(horizontal = 5.dp)
+                            .padding(bottom = 5.dp)
+                    )
+                    Button(
                         onClick = {
-                                  onDownloadButtonClicked(item.id, item)
-                        },
+                            onDownloadButtonClicked(item.id, item)
+                                  },
+                        Modifier
+                            .align(alignment = Alignment.CenterEnd)
+                            .padding(end = 5.dp),
                         enabled = !downloaded
                     ){
                         Text(buttonText.value)
+                        //Icon(contentDescription = "", painter = painterResource(R.drawable.ic_baseline_file_download_24))
                     }
                 }
-
             }
         }
     }
-
-
 }
 
 private fun isLocallyAvailable(id: Int, localTutorials: List<com.tutorialapp.data.database.Tutorial>) : Boolean{
